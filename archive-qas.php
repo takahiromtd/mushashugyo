32ab10b32c93bdc023b3275921c489e2cc518b39
takahiromtd <takahiro.mtd@gmail.com>
2018-06-11 18:36:55 +0900

    tiny fix

--- archive-qas.php
+++ archive-qas.php
@@ -14,66 +14,66 @@ Template Name: archive-qas.php
 			<div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6">
 				<h3 class="font-weight-bold"><b>プログラム説明会について</b></h3>
 				<ul>
-					<li><a href="/qas/event-entry/3">プログラム説明会の参加日程の変更はできますか？</a></li>
-					<li><a href="/qas/event-entry/4">海外留学中ですがプログラムの説明を受けることはできますか？</a></li>
+					<li><a href="/qas/3">プログラム説明会の参加日程の変更はできますか？</a></li>
+					<li><a href="/qas/4">海外留学中ですがプログラムの説明を受けることはできますか？</a></li>
 				</ul>
 				<h3 class="font-weight-bold">プログラム内容について</h3>
 				<ul>
-					<li><a href="/qas/event-entry/6">就職活動を終えた4年生でも参加できますでしょうか？</a></li>
-					<li><a href="/qas/event-entry/10">なぜ海外で行う必要があるのですか?</a></li>
-					<li><a href="/qas/event-entry/11">なぜベトナムが開催地なんですか？</a></li>
-					<li><a href="/qas/event-entry/12">外国人とコミュニケーションを取る機会はありますか？</a></li>
-					<li><a href="/qas/event-entry/13">医学部生でも参加できますか？</a></li>
-					<li><a href="/qas/event-entry/14">理系でも参加できますか？</a></li>
-					<li><a href="/qas/event-entry/15">エンジニア志望ですが参加できますか？</a></li>
-					<li><a href="/qas/event-entry/16">就職活動がはじまってしまいますが参加できますか？</a></li>
-					<li><a href="/qas/event-entry/17">プログラムの日程を教えてください。</a></li>
-					<li><a href="/qas/event-entry/18">ビジネスコンテストとの違いはなんですか？</a></li>
-					<li><a href="/qas/event-entry/19">学部1年生でも参加できますか？</a></li>
-					<li><a href="/qas/event-entry/20">語学留学との違いはなんですか？</a></li>
-					<li><a href="/qas/event-entry/21">英語力はどれぐらい必要ですか？</a></li>
-					<li><a href="/qas/event-entry/22">なぜ学生がビジネスを学ぶ必要があるのですか？</a></li>
-					<li><a href="/qas/event-entry/23">プログラムに参加してビジネスの知識は得られますか？</a></li>
-					<li><a href="/qas/event-entry/24">大学でビジネスを専攻している訳ではないのですが、参加できますか？</a></li>
-					<li><a href="/qas/event-entry/25">日本でやっている他のインターンシップとの違いを教えてください。</a></li>
-					<li><a href="/qas/event-entry/26">現在専門学校に通っているのですが、専門学生でも参加できますか？</a></li>
-					<li><a href="/qas/event-entry/28">15日間のプログラム期間中、途中参加・帰国することはできますか？</a></li>
-					<li><a href="/qas/event-entry/29">プログラムはタームあたり何人で実施するのでしょうか？</a></li>
-					<li><a href="/qas/event-entry/34">期間中はずっと自分の企画やプロジェクトを行うのですか？</a></li>
-					<li><a href="/qas/event-entry/38">参加したいのですが、親の許可がとれなさそうです。</a></li>
-					<li><a href="/qas/event-entry/39">自分のやりたいことではないかもしれないと思っているのですがどうでしょうか？</a></li>
-					<li><a href="/qas/event-entry/49">将来海外で働くことの役に立ちますか？</a></li>
-					<li><a href="/qas/event-entry/50">武者修行の経験は、就職活動の場面でどのように役立ちますか？</a></li>
-					<li><a href="/qas/event-entry/53">社会人になってから武者修行で学んだことがどのように活きますか？</a></li>
-					<li><a href="/qas/event-entry/54">プログラムには高校生も参加できますか？</a></li>
+					<li><a href="/qas/6">就職活動を終えた4年生でも参加できますでしょうか？</a></li>
+					<li><a href="/qas/10">なぜ海外で行う必要があるのですか?</a></li>
+					<li><a href="/qas/11">なぜベトナムが開催地なんですか？</a></li>
+					<li><a href="/qas/12">外国人とコミュニケーションを取る機会はありますか？</a></li>
+					<li><a href="/qas/13">医学部生でも参加できますか？</a></li>
+					<li><a href="/qas/14">理系でも参加できますか？</a></li>
+					<li><a href="/qas/15">エンジニア志望ですが参加できますか？</a></li>
+					<li><a href="/qas/16">就職活動がはじまってしまいますが参加できますか？</a></li>
+					<li><a href="/qas/17">プログラムの日程を教えてください。</a></li>
+					<li><a href="/qas/18">ビジネスコンテストとの違いはなんですか？</a></li>
+					<li><a href="/qas/19">学部1年生でも参加できますか？</a></li>
+					<li><a href="/qas/20">語学留学との違いはなんですか？</a></li>
+					<li><a href="/qas/21">英語力はどれぐらい必要ですか？</a></li>
+					<li><a href="/qas/22">なぜ学生がビジネスを学ぶ必要があるのですか？</a></li>
+					<li><a href="/qas/23">プログラムに参加してビジネスの知識は得られますか？</a></li>
+					<li><a href="/qas/24">大学でビジネスを専攻している訳ではないのですが、参加できますか？</a></li>
+					<li><a href="/qas/25">日本でやっている他のインターンシップとの違いを教えてください。</a></li>
+					<li><a href="/qas/26">現在専門学校に通っているのですが、専門学生でも参加できますか？</a></li>
+					<li><a href="/qas/28">15日間のプログラム期間中、途中参加・帰国することはできますか？</a></li>
+					<li><a href="/qas/29">プログラムはタームあたり何人で実施するのでしょうか？</a></li>
+					<li><a href="/qas/34">期間中はずっと自分の企画やプロジェクトを行うのですか？</a></li>
+					<li><a href="/qas/38">参加したいのですが、親の許可がとれなさそうです。</a></li>
+					<li><a href="/qas/39">自分のやりたいことではないかもしれないと思っているのですがどうでしょうか？</a></li>
+					<li><a href="/qas/49">将来海外で働くことの役に立ちますか？</a></li>
+					<li><a href="/qas/50">武者修行の経験は、就職活動の場面でどのように役立ちますか？</a></li>
+					<li><a href="/qas/53">社会人になってから武者修行で学んだことがどのように活きますか？</a></li>
+					<li><a href="/qas/54">プログラムには高校生も参加できますか？</a></li>
 				</ul>
 			</div>
 			<div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6" >
 				<h3 class="font-weight-bold"><b>渡航・現地生活について</b></h3>
 				<ul>
-					<li><a href="/qas/event-entry/8">滞在先の治安について教えてください。</a></li>
-					<li><a href="/qas/event-entry/9">現地の滞在先のホテルは安全ですか？</a></li>
-					<li><a href="/qas/event-entry/30">参加するのにビザは必要ですか？</a></li>
-					<li><a href="/qas/event-entry/31">現地で充電や家電製品は利用できますか？</a></li>
-					<li><a href="/qas/event-entry/32">現地集合と聞きましたが、一人旅は不安です...</a></li>
-					<li><a href="/qas/event-entry/33">プログラム期間中、子ども（参加者）と連絡がとれますか？</a></li>
-					<li><a href="/qas/event-entry/35">万が一、病気・怪我、トラブルが起きた場合、どのような対応をしてくれますか？</a></li>
-					<li><a href="/qas/event-entry/36">大学が仲介しているプログラムの方が安全なのではないでしょうか？</a></li>
-					<li><a href="/qas/event-entry/43">ホテルにwi-fiはついていますか？</a></li>
-					<li><a href="/qas/event-entry/44">海外旅行保険には、加入した方が良いですか？</a></li>
-					<li><a href="/qas/event-entry/47">PC持参は必須なのでしょうか？</a></li>
-					<li><a href="/qas/event-entry/52">携帯電話は使えますか？</a></li>
+					<li><a href="/qas/8">滞在先の治安について教えてください。</a></li>
+					<li><a href="/qas/9">現地の滞在先のホテルは安全ですか？</a></li>
+					<li><a href="/qas/30">参加するのにビザは必要ですか？</a></li>
+					<li><a href="/qas/31">現地で充電や家電製品は利用できますか？</a></li>
+					<li><a href="/qas/32">現地集合と聞きましたが、一人旅は不安です...</a></li>
+					<li><a href="/qas/33">プログラム期間中、子ども（参加者）と連絡がとれますか？</a></li>
+					<li><a href="/qas/35">万が一、病気・怪我、トラブルが起きた場合、どのような対応をしてくれますか？</a></li>
+					<li><a href="/qas/36">大学が仲介しているプログラムの方が安全なのではないでしょうか？</a></li>
+					<li><a href="/qas/43">ホテルにwi-fiはついていますか？</a></li>
+					<li><a href="/qas/44">海外旅行保険には、加入した方が良いですか？</a></li>
+					<li><a href="/qas/47">PC持参は必須なのでしょうか？</a></li>
+					<li><a href="/qas/52">携帯電話は使えますか？</a></li>
 				</ul>
 					<h3 class="font-weight-bold"><b>運営会社旅武者について</b></h3>
 					<ul>
-						<li><a href="/qas/event-entry/1">株式会社旅武者の設立年数はどれぐらいですか？</a></li>
-						<li><a href="/qas/event-entry/2">武者修行プログラムは何年間行っているものですか？</a></li>
+						<li><a href="/qas/1">株式会社旅武者の設立年数はどれぐらいですか？</a></li>
+						<li><a href="/qas/2">武者修行プログラムは何年間行っているものですか？</a></li>
 					</ul>
 				<h3 class="font-weight-bold"><b>料金・支払い、その他</b></h3>
 				<ul>
-					<li><a href="/qas/event-entry/1">プログラムに参加にかかる費用はいくらですか？</a></li>
-					<li><a href="/qas/event-entry/1">参加したいのですが、お金の工面が難しいです。</a></li>
-					<li><a href="/qas/event-entry/1">プログラムを支援して公認している大学、または団体はありますか？また、ある場合はどこの大学、団体か教えてください。</a></li>
+					<li><a href="/qas/1">プログラムに参加にかかる費用はいくらですか？</a></li>
+					<li><a href="/qas/1">参加したいのですが、お金の工面が難しいです。</a></li>
+					<li><a href="/qas/1">プログラムを支援して公認している大学、または団体はありますか？また、ある場合はどこの大学、団体か教えてください。</a></li>
 				</ul>
 			</div>
 		</div>
