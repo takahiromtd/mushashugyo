d688ef936627992551667c6f274983af74248d65
takahiromtd <takahiro.mtd@gmail.com>
2019-06-11 13:02:27 +0900

    update

---
+++ page-ft.php
@@ -0,0 +1,257 @@
+<?php
+/*
+Template Name: page-ft.php
+*/
+?>
+<?php get_header(); ?>
+<div class="jumbotron">
+	<h2 class="text-center font-weight-bold"><?php echo get_the_title(); ?></h2>
+	<p class="text-center"><?php echo get_post_meta($post->ID , 'wpcf-page-sub-title' ,true); ?></p>
+</div>
+<div class="container pt-5 pb-5">
+<div id="facilitator">
+<section class="mw960 mx-auto d-block">
+<h2 class="text-center text-danger font-weight-bold">ファシリテーターとは自立を促す人</h2>
+<p>ビジネスをほとんど経験したことのない学生が、チームでビジネス企画を行うため、その過程で壁にぶつかることがあります。そのようなとき、ビジネス企画やチームビルディングのプロであるファシリテーターがサポートします。しかし、ファシリテーターは「答え」を教えることはしません。あくまでも、問いかけやワークによって「考え方」や「自分の見つめ方」を伝えます。それは、プログラムを終えた後にも再現可能な「本質的な力」を身につけることを意図しています。旧来の学校教育の「知識を与え、覚える」ことを主とした教育ではなく、参加者の皆さんが『自走式エンジンⓇ』を積むことが将来の可能性の発展につながると考えています。</p>
+</section>
+<div class="mw960 mx-auto d-block">
+<div class="embed-responsive embed-responsive-16by9 mt-5 mb-5">
+<iframe class="embed-responsive-item" src="https://www.youtube.com/embed/Eq1pZ1eB6aM"></iframe>
+</div>
+</div>
+<section class="mw960 mx-auto d-block">
+<h2 class="text-center text-danger font-weight-bold">ビジネスとリーダーシップ開発のプロが成長をサポート</h2>
+<p>異なる役割・専門性を持った複数のファシリテーターで、1つのタームを支援します。</p>
+<p>通常のインターンシップは、社員がサポートしますが、彼らは人材育成のプロではありません。武者修行では、プロコーチであり、大手企業でリーダーシップ研修などを行なう『チームビルディングファシリテーター』、戦略系コンサルタントなどの経験を持つ『ビジネスファシリテーター』が連携し、効果的なプログラムを提供します。ただ『経験する』ということだけでは、そこから学びにつなげていけるかどうかは個人に委ねられてしまいます。チームビルディングファシリテーターの関わりが、確実に経験からの内省を促し、学び・変態につなげます。ビジネスのプロであるビジネスファシリテーターは、セオリーや考え方は示すものの、答えは教えず、自分たちの力で考えることをサポートしてくれます。自分で失敗や成功を経験するからこそ、学びが自分のものになります</p>
+<div class="row mt-5">
+<div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6">
+<h3 class="font-weight-bold pb-1">チームビルディングファシリテーター</h3>
+<div class="bg-light p-3">
+<p>多くは企業の研修講師として活躍している人財育成のプロです。人間関係づくりや、リーダーシップに関してコーチングやワークショップを通じてサポートします。</p>
+</div>
+</div>
+<div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6">
+<h3 class="font-weight-bold pb-1">ビジネスファシリテーター</h3>
+<div class="bg-light p-3">
+<p>経営者や起業家、コンサルティング企業出身者が担います。企画へのフィードバックやビジネス上の視点への気付きを促し、参加者の企画の推進をサポートします。</p>
+</div>
+</div>
+</div>
+<div class="row">
+<div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6">
+<h3 class="font-weight-bold pb-1">現地コーディネーター</h3>
+<div class="bg-light p-3">
+<p>プログラム中参加者と帯同し現地での日々の生活、病気やトラブルが起きてしまった場合のサポートをします。</p>
+</div>
+</div>
+<div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6">
+<h3 class="font-weight-bold pb-1">カントリーヘッド</h3>
+<div class="bg-light p-3">
+<p>弊社ベトナム法人の社長です。ベトナムの店鋪経営の総責任者として参加者と関わります。</p>
+</div>
+</div>
+</div>
+</section>
+<section class="mx-auto d-block">
+<h2 class="text-center text-danger font-weight-bold font-weight-bold">ファシリテーターの紹介</h2>
+<div class="row">
+<div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4 mb-5">
+<div class="ovflh-box">
+<div class="w-50 float-left p-2">
+<img class="img-fluid mx-auto d-block" src="[theme_url]/img/facili-murakami.jpg" width="420">
+</div>
+<div class="w-50 float-left p-2">
+<p class="font-weight-bold">村上卓</p>
+<p class="fs-small">村上卓事務所 代表 / 組織・人財開発コンサルタント<br>米国CTI認定プロコーチ（CPCC)<br>CTIジャパン・リーダーシッププログラム 修了</p>
+</div>
+</div>
+<p class="d-none d-lg-block fs-small">1982年 山口県生まれ。広島大学を卒業後、広島大学大学院 生物圏科学研究科にて産学官連携研究員として勤務。リクルートグループに転じ、中小企業の新卒・中途社員採用支援に従事。関西HRカンパニーにおいて新規開拓MVPを獲得。その後、人財開発コンサルティング会社にて、数多くの大手企業の人財育成、組織力強化に従事。2013年 持続可能な社会へのシフト促進に向け、企業や地域の創造性やリーダーシップ発揮の支援に注力するために、独立。リアルなチームや組織に関わり、ビジネスや現場を変えていくことを重視している。また、若くして、大手企業のマネジャーやベテラン社員を対象とした多くの場をリードしてきている。バンコクでの新規事業立上げなど、東南アジアでのビジネス経験もある。論文：『Producing human ceramide-NS by metabolic engineering using yeast Saccharomyces cerevisiae』（www.nature.com/articles/srep16319）</p>
+</div>
+<div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4 mb-5">
+<div class="ovflh-box">
+<div class="w-50 float-left p-2">
+<img class="img-fluid mx-auto d-block" src="[theme_url]/img/facili-maeda.jpg" width="420">
+</div>
+<div class="w-50 float-left p-2">
+<p class="font-weight-bold">前田 恵一</p>
+<p class="fs-small">株式会社レゾナンス 代表</p>
+</div>
+</div>
+<p class="d-none d-lg-block fs-small">2005年　東京大学大学院 情報理工学系研究科 修了。数理統計的アプローチによる遺伝子の特徴量抽出を研究。IBMビジネスコンサルティングサービスにて、ERPパッケージによる会計システム導入や、新人研修に従事。野村證券（グローバルマーケッツ部門）に転じ、国内機関投資家向けにクオンツモデル情報や数百銘柄の同時トレードを行うバスケットトレーディングサービスに従事。2011年　レゾナンス社設立。大学生向けキャリア形成プログラムやアプリ開発・Web開発の受託開発事業に携わった後、ベンチャー向け新卒・第二新卒採用支援に従事大学生を対象とした私塾「前田塾」を主宰。会計、経済、金融、政治、統計学、Excelなど、ビジネスに必要な素養全般をチームディスカッション形式で学ぶ。</p>
+</div>
+<div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4 mb-5">
+<div class="ovflh-box">
+<div class="w-50 float-left p-2">
+<img class="img-fluid mx-auto d-block" src="[theme_url]/img/facili-kato.jpg" width="420">
+</div>
+<div class="w-50 float-left p-2">
+<p class="font-weight-bold">加藤 大吾</p>
+<p class="fs-small">NPO法人 都留環境フォーラム 代表理事<br>CTIジャパン・リーダーシッププログラム 修了</p>
+</div>
+</div>
+<p class="d-none d-lg-block fs-small">1973年生まれ。幼少から青年期を東京で暮らす。NPO勤務を経て、2001年 環境教育事業所「アースコンシャス」を設立。東京の暮らしに疑問を感じ、やるべき事と居るべき場所を求めて家族で土地を探しはじめる。2005年に愛・地球博にて森の自然学校・里の自然学校チーフなどを務めながら、山梨県都留市の山林を友人たちと開拓、セルフビルドの国産丸太小屋に移住。環境教育者、企業研修講師、農家、大学非常勤講師など多彩な分野を持ちながら3人の子どもと40羽のニワトリと4頭の綿羊と農耕馬に囲まれ、田んぼや畑を耕作し、生態系の中で暮らす。2010年「NPO法人都留環境フォーラム」設立。環境配慮した地域作りを開始。在来野菜のタネの保存活動、馬で田畑を耕す技術の伝承活動など多肢に渡る社会問題への活動を展開する。現在では、日本が先進国になる過程での学びや文化、自らの暮らしぶりを途上国に伝えることで平和と幸せ感に貢献することに力を注ぐ。<a href="http://www.earth-c.info">著書：『地球に暮らそう 〜生態系の中に生きるという選択肢〜』（旅と冒険社）</a> など</p>
+</div>
+<div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4 mb-5">
+<div class="ovflh-box">
+<div class="w-50 float-left p-2">
+<img class="img-fluid mx-auto d-block" src="[theme_url]/img/facili-sato.jpg" width="420">
+</div>
+<div class="w-50 float-left p-2">
+<p class="font-weight-bold">佐藤 ココア</p>
+<p class="fs-small">オフィスAlive 代表 / 人材開発コンサルタント、ラーニングファシリテーター<br>米国CTI認定プロコーチ： Certified Professional Co-Active Coach (CPCC)<br>米国CCE, Inc. 認定GCDFキャリアカウンセラー</p>
+</div>
+</div>
+<p class="d-none d-lg-block fs-small">大手新聞社の事業開発部門スタッフとして、スポーツクラブ事業やイベント事業の立ち上げから成長をサポートする。その後、特定非営利活動法人（NPO）に入団。ダイバーシティの考え方にたち、すべての人々がいきいき暮らせる社会の実現を目指した交流イベントの立ち上げに携わる。また、ダイバーシティをテーマにした企業向け研修プログラムの企画運営にも従事。2005年、人材育成コンサルティング企業に入社。研修コンテンツ開発、アセスメント開発、ワークショップデザインを担当し、主軸商品を多数開発。並行して、企業研修やワークショップの講師、ファシリテーターとして多数登壇。個人向けキャリアカウンセリング、コーチング、アセスメントプロファイリングも実施。2013年、オフィスAlive設立。研修・ワークショップの講師、ファシリテーターとして、個人と組織の潜在力を引き出す支援を行っている。<br>オフィシャルサイト：<a href="http://ayako310.com/">http://ayako310.com/</a></p>
+</div>
+<div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4 mb-5">
+<div class="ovflh-box">
+<div class="w-50 float-left p-2">
+<img class="img-fluid mx-auto d-block" src="[theme_url]/img/facili-kengo.jpg" width="420">
+</div>
+<div class="w-50 float-left p-2">
+<p class="font-weight-bold">三木 健吾</p>
+<p class="fs-small">株式会社U360　代表取締役</p>
+</div>
+</div>
+<p class="d-none d-lg-block fs-small">1984年生まれ、京都在住、ヨガ歴２年。慶應義塾大学SFCにて東アジアの国際関係論を学ぶ。</p>
+<p class="d-none d-lg-block fs-small">アビームコンサルティングにて業務・ITコンサルタント、ブルーカレント・ジャパンにてPRプランナーに従事。</p>
+<p class="d-none d-lg-block fs-small">独立・起業後はスポーツ事業者向けマーケティング、シェフ出張料理、ビリヤニ（北インド発祥のエスニック理料理）などの事業立ち上げ・運営を行う。</p>
+<p class="d-none d-lg-block fs-small">2014年に株式会社U360（ユーサンロクマル）を設立し、現在は新規事業立ち上げに関するコンサルティングやワークショップ・研修のファシリテーション、日本ビリヤニ協会の運営を行っている。</p>
+<p class="d-none d-lg-block fs-small">2018年より京都大学経営管理大学院デザインプログラムにて事業デザイン、経営教育をテーマとした探求活動に従事。</p>
+<p class="d-none d-lg-block fs-small">編集協力：安原智樹著「マーケティングの基本」（日本実業出版社）。
+</div>
+<div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4 mb-5">
+<div class="ovflh-box">
+<div class="w-50 float-left p-2">
+<img class="img-fluid mx-auto d-block" src="[theme_url]/img/facili-tsururu.jpg" width="420">
+</div>
+<div class="w-50 float-left p-2">
+<p class="font-weight-bold">津留 沢美</p>
+<p class="fs-small">米国CTI認定プロコーチ（CPCC）<br>CTIジャパン・リーダーシッププログラム 修了<br>米国CCE, Inc. 認定GCDFキャリアカウンセラー</p>
+</div>
+</div>
+<p class="d-none d-lg-block fs-small">大学卒業後、大手学習塾に入社。教科指導、研修や勉強会の企画・運営等に従事。その後米国ウィスコンシン州の現地小学校にて1年間日本語教師をしながらUniversity of Wisconsin Stevens Pointで学ぶ。帰国後、チリの船会社「Compania Chilena de Navegacion Intereoceanica」、クウェートの船会社「United Arab Shipping」にて極東Repの秘書業務に従事。その後バイオベンチャーに立上げ時に入社し、社長秘書業務から総務、人事など携わり、立上げから3年でマザーズ上場を果たす。2004年度より株式会社ウィル・シードにて学校事業のオペレーションを担当。2007 年より企業向け研修の企画・設計から講師への説明、自らも講師として登壇し、研修一連の業務に従事。2011年より営業部にて顧客へのソリューション提案、新規顧客営業にも従事する。2014年1月 独立。</p>
+</div>
+<div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4 mb-5">
+<div class="ovflh-box">
+<div class="w-50 float-left p-2">
+<img class="img-fluid mx-auto d-block" src="[theme_url]/img/facili-kain.jpg" width="420">
+</div>
+<div class="w-50 float-left p-2">
+<p class="font-weight-bold">Khanh N. Nguyen（カイン）</p>
+<p class="fs-small">Tabi-Musha Vietnam カントリーヘッド</p>
+</div>
+</div>
+<p class="d-none d-lg-block fs-small">米国の大学 UCLA（University of California, Los Angeles）を卒業。サムスン（ベトナム）のマーケターとして、薄型テレビをベトナムに導入。ベトナム市場で初のシェアNo.1を実現する。ベトナムのスポーツエンターテインメント事業会社 XLEに転じ、プロバスケットボールチームのSaigon Heatのマーケターとして、メディアやPR活動に従事。2014年より武者修行プログラムに関わるようになり、現在は山口にヘッドハントされ、旅武者ベトナム法人のカントリーヘッドを担う。</p>
+</div>
+<div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4 mb-5">
+<div class="ovflh-box">
+<div class="w-50 float-left p-2">
+<img class="img-fluid mx-auto d-block" src="[theme_url]/img/facili-odagiri.jpg" width="420">
+</div>
+<div class="w-50 float-left p-2">
+<p class="font-weight-bold">小田桐 翔大</p>
+<p class="fs-small">株式会社FLYHIGH代表取締役<br>メンタルコーチ / ファシリテーター</p>
+</div>
+</div>
+<p class="d-none d-lg-block fs-small">1985年青森県生まれ。小学校から高校までプロ野球選手・甲子園を目指し、野球に明け暮れる。1年間の浪人生活を経て、筑波大学体育専門学群に入学。新たな競技でトップレベルの環境に挑戦したいと決意し、初心者で体育会バスケットボール部に入部。全国優勝経験者など全国トップクラスのメンバーの中、チームメイトにも支えられ、4年間プレーヤーとして在籍。全国ベスト８も経験。大学卒業後、(株)JTB法人東京に入社し、法人営業、国内・海外添乗員を4年間担務。大手一流企業から専門学校まで幅広く担当し、優秀営業成績賞も受賞。2013年1月にアスリートの力になりたいという想いでメンタルコーチとして独立。ソチ五輪リュージュ日本代表金山英勢選手、ラート日本代表チームのメンタルコーチや日本郵便株式会社の企業研修など幅広く活動。2016年3月8日に法人化し、株式会社FLYHIGHを設立（<a href="http://flyhigh-works.jp/">https://flyhigh-works.jp</a>）。東京を拠点にメンタルコーチ・ファシリテーターとして活動中。</p>
+</div>
+<div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4 mb-5">
+<div class="ovflh-box">
+<div class="w-50 float-left p-2">
+<img class="img-fluid mx-auto d-block" src="[theme_url]/img/facili-nishio.jpg" width="420">
+</div>
+<div class="w-50 float-left p-2">
+<p class="font-weight-bold">西尾信一</p>
+<p class="fs-small">ベア特定社会保険労務士事務所　代表<br>特定社会保険労務士<br>ビジネスファシリテーター<br>ワークショップデザイナー育成プログラム　修了</p>
+</div>
+</div>
+<p class="d-none d-lg-block fs-small">獨協大学卒業。大手会計事務所人事コンサルティング部門に就職し外資系企業20社の給与・労務コンサルティングを実施。デル株式会社人事部を経てSAPジャパン株式会社に転職。ERPのプリセールス、人事コンサルティング、自社の採用タスクフォースチーム責任者を歴任。アビームコンサルティング株式会社にてチェンジマネジメント、海外支社の業務改革プロジェクト等を経て、トレンドマイクロ株式会社に転職。人事企画チーム、採用チームの責任者を兼任し人事制度導入、及び年間約100人の中途採用と約25人の新卒採用スキームの企画・立案・実施を行う。2014年にベア特定社会保険労務士事務所を設立。現在は主に外資系企業の人事部マネージャ代行、変革ワークショップのファシリテーター、ワークショップショップの開発、キャリアカウンセリングを行う。また、企業のビジネスプロセスを分析し、シナジーを創出来る企業間のコラボレーショを構築するビジネスコーディネータとしても活躍する。</p>
+</div>
+<div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4 mb-5">
+<div class="ovflh-box">
+<div class="w-50 float-left p-2">
+<img class="img-fluid mx-auto d-block" src="[theme_url]/img/facili-kuroda.jpg" width="420">
+</div>
+<div class="w-50 float-left p-2">
+<p class="font-weight-bold">倉岡 麻美子</p>
+<p class="fs-small">PETHICA合同会社 代表<br>デザイナー兼クリエイティブディレクター</p>
+</div>
+</div>
+<p class="d-none d-lg-block fs-small">2015年ジュエリーブランド「PETHICA（<a href="https://www.pethicajewelry.com/">https://www.pethicajewelry.com/</a>）」を立ち上げる。自身が世界中を旅した経験のストーリーから伝える「旅するジュエリー」をコンセプトに事業展開中。ストーリーを通じて生まれる笑顔や、心の豊かさを作品と共に提案している。<br>また、2017年より、世界を知り、課題を見つけ、平和へ繋げることをミッションとした活動「Cinema　PETHICA」を運営開始。都内ギャラリーにてドキュメンタリー映画会を毎月開催中。<br>なによりも旅が好きで、特に辺境好き。現地の人と仲良くなるのが得意。登山が趣味で、アルプス縦走や森の中にいるときが幸せ。</p>
+</div>
+<div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4 mb-5">
+<div class="ovflh-box">
+<div class="w-50 float-left p-2">
+<img class="img-fluid mx-auto d-block" src="[theme_url]/img/facili-ishii.jpg" width="420">
+</div>
+<div class="w-50 float-left p-2">
+<p class="font-weight-bold">石井 宏明</p>
+<p class="fs-small">HEART AND BRAIN COMMUNICATION代表<br>人材育成・組織開発ファシリテーター/プロコーチトレーナー（CTIジャパン・CRRジャパン ファカルティ）</p>
+</div>
+</div>
+<p class="d-none d-lg-block fs-small">1994年 外資系SI企業（EDSジャパン：現HP）に入社。SEとして世界10ヶ国を巡る金融系グローバルプロジェクト担当。その後、外資系コンサルティングファーム（ケンブリッジ・テクノロジー・パートナーズ）に転職。プロジェクトマネージャーとして大手メーカーの国内外プロジェクトを複数担当し、2年連続の社長賞を受賞。2003年 会社の数字はあがっても、働く人がもう一つ幸せになっていない多くの日本企業に疑問を持ち、コンサルティングの世界からコーチングの世界へとキャリアチェンジ。ジャパンビジネスラボ取締役及び我究館副館長に就任。社会人のキャリアデザイン、特にキャリアチェンジのサポートを得意とする。2008年独立し「HEART AND BRAIN COMMUNICATION」を設立。”活きる”人・”活きる”組織づくりをモットーに、企業への人材教育やシステムコーチング、個人へのキャリアデザイン支援やパーソナルコーチングを中心に活動中。</p>
+</div>
+<div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4 mb-5">
+<div class="ovflh-box">
+<div class="w-50 float-left p-2">
+<img class="img-fluid mx-auto d-block" src="[theme_url]/img/naoharada.png" width="420">
+</div>
+<div class="w-50 float-left p-2">
+<p class="font-weight-bold">原田 直和</p>
+<p class="fs-small">株式会社Heart To Earth 代表取締役<br>CRRジャパン ファカルティ・プロコーチトレーナー<br>国際コーチ連盟認定PCC</p>
+</div>
+</div>
+<p class="d-none d-lg-block fs-small">1975年 山口県生まれ。大学卒業後、外資系製薬会社へ入社。医薬情報担当者時代では主力製品シェアNo1に拘り、東大病院を始め担当する多くの地域や施設でNo1シェアを獲得する。また本社営業戦略推進部でチーム制導入プロジェクトやナレッジマネジメントなどのプロジェクトをリードし、15年間営業・マーケティング・チームをテーマに会社員人生を送る。人と人の関係性が成果に大きく影響を与えること経験し「誰もが自由にイキイキと生きれる持続可能な社会・組織創り」を目指し、2014年3月11日「株式会社Heart To Earth」設立。企業やNPOの経営チームやプロジェクトチームに対するシステムコーチングや、経営者や医師に対するコーチングを提供している。また、国際コーチ連盟認定機関であるCRRジャパンのファカルティとして、プロコーチやファシリテーターの育成に従事。現在、医学系専門誌「看護管理」（医学書院）にて2018年1月号より「コーチングと組織の関係性システムから考えるコミュニケーション」にて連載中。</p>
+</div>
+<div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4 mb-5">
+<div class="ovflh-box">
+<div class="w-50 float-left p-2">
+<img class="img-fluid mx-auto d-block" src="[theme_url]/img/hirayamayuzo.png" width="420">
+</div>
+<div class="w-50 float-left p-2">
+<p class="font-weight-bold">平山 裕三</p>
+<p class="fs-small">株式会社はぐくむ<br>NPO法人バブリング<br>ライフワークコーチ<br>LGBT / ダイバーシティキャリアカウンセラー</p>
+</div>
+</div>
+<p class="d-none d-lg-block fs-small">1985年生まれ。幼少期から恋愛対象が同性だと自覚。しかし、中高時代はそれを周りに言えず悩み苦しむ青春時代を過ごす。本当の自分として幸せな人生を送ることを諦めていたが、大学時代に株式会社はぐくむと出会い、「これから自分はどう生きていきたいか？」を考え始め、「カミングアウトし、あるがままに生きていきたい」と決意する。</p>
+<p class="d-none d-lg-block fs-small">友人や家族に伝え、更に個人blogにて「僕がゲイだとカミングアウトする理由」を書いたことで、SNSで拡散され、大きな反響を呼ぶ。</p>
+<p class="d-none d-lg-block fs-small">大学卒業後、ソフトバンクにて法人営業を担当し、東京エリアで1位の成績を収める。また、孫正義氏の後継者を育てるソフトバンクアカデミア第1期生に選ばれ、ビジネススキルや起業家としての生き方を学ぶ。</p>
+<p class="d-none d-lg-block fs-small">ソフトバンク退社後、はぐくむへ入社。年間500人近い大学生や社会人、経営者にコーチングやカウンセリングを行う日々をおくる。また週末には、NPO法人バブリングにて、様々なマイノリティの方々が生きやすい社会を築くため活動している。</p>
+</div>
+<div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4 mb-5">
+<div class="ovflh-box">
+<div class="w-50 float-left p-2">
+<img class="img-fluid mx-auto d-block" src="[theme_url]/img/hatanotakafumi.png" width="420">
+</div>
+<div class="w-50 float-left p-2">
+<p class="font-weight-bold">波多野 貴史</p>
+<p class="fs-small">チームビルディング・ファシリテーター<br>関西大学 人間健康学部 非常勤講師<br>エンディコット大学 スポーツマネージメント卒（マサチューセッツ州、ベバリー市）</p>
+</div>
+</div>
+<p class="d-none d-lg-block fs-small">20歳からカナダ→アメリカ→ハワイで13年間生活しながら、教育機関・ボランティア等を通じてアドベンチャープログラムなどの野外教育学習を体系的に学ぶ。大学在学中４年間アドベンチャー・カウンセリングを活用した人材育成・自己啓発プログラムにファシリテーターとして修行し続け、大学卒業後メイン州にあるLL BEAN本部・Outdoor Discovery Program Departmentにてファシリテーターを努めた後にハワイのYMCA Camp Erdmanに転職。特にハワイのYMCAではディレクターとしてファシリテーターを養成すると同時に、数千にも及ぶ団体に対してファシリテーションを実施してきた。</p>
+<p class="d-none d-lg-block fs-small">2001年衝撃的な9・11テロ事件をキッカケに帰国。帰国後大分県でNPO法人ABC野外教育センターの立ち上げと軌道に乗せるためにマネージャーを務めファシリテーターを育成しながらアドベンチャー教育を活用したリーダーシップ育成やチームビルディング・プログラムを行い、企業研修や青少年育成に力を注ぐ。現在は『tryworld』代表としてアドベンチャー教育を活かした人材育成・自己啓発プログラムのファシリテーターとして日本全国を奔走中。</p>
+</div>
+<div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4 mb-5">
+<div class="ovflh-box">
+<div class="w-50 float-left p-2">
+<img class="img-fluid mx-auto d-block" src="[theme_url]/img/akane.png" width="420">
+</div>
+<div class="w-50 float-left p-2">
+<p class="font-weight-bold">徳永 茜</p>
+<p class="fs-small">コーディネーター</p>
+</div>
+</div>
+<p class="d-none d-lg-block fs-small">高校時代、アメリカ ワシントン州立 HAZEN HIGH SCHOOL 留学</p>
+<p class="d-none d-lg-block fs-small">立命館アジア太平洋大学 アジア太平洋学部 卒 内定先ウエディング会社の、社会貢献プロジェクトリーダを務める(カンボジア渡航) 2009年~2012年ウエディングプランナーで営業・企画を学び、 2012年に本社営業企画部へ異動 本社在職中、社内社会貢献プロジェクトに再びメンバーとして参画(ラオス渡航) 2013年一般社団法人re:terraへ転身し、単身カンボジアへ移住</p>
+<p class="d-none d-lg-block fs-small">東南アジア美容プロジェクトのボードメンバーとして、 クラウドファンディングready for?で1,500,000円の資金調達を担当</p>
+<p class="d-none d-lg-block fs-small">NPO法人シェアードスマイルジャパンで現地コーディネイターとして、 東南アジア各国の学校建設やベトナム職業訓練学校での日本語コース運営に従事 また、学校建設を通じた、企業向けの内定者研修も担当</p>
+<p class="d-none d-lg-block fs-small">2015年春から、東南アジアでのリユースビジネスのプロジェクトメンバーとして活動</p>
+</div>
+</div>
+</section>
+</div>
+</div>
+<?php get_template_part('footer-navigation'); ?>
+<?php get_template_part('footer-entry'); ?>
+<?php get_footer(); ?>
