54c0882385ee2d9ad663119b7eed337fa788d184
takahiromtd <takahiro.mtd@gmail.com>
2020-10-29 15:44:09 +0900

    update

--- header-tags.php
+++ header-tags.php
@@ -37,25 +37,5 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 <noscript><img height="1" width="1" style="display:none"
   src="https://www.facebook.com/tr?id=1418147948473894&ev=PageView&noscript=1"
 /></noscript>
-<!-- End Facebook Pixel Code -->
-<!-- Facebook Pixel Code takahiromtd -->
-<script>
-  !function(f,b,e,v,n,t,s)
-  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-  n.queue=[];t=b.createElement(e);t.async=!0;
-  t.src=v;s=b.getElementsByTagName(e)[0];
-  s.parentNode.insertBefore(t,s)}(window, document,'script',
-  'https://connect.facebook.net/en_US/fbevents.js');
-  fbq('init', '143161029545703');
-  fbq('track', 'PageView');
-</script>
-<noscript><img height="1" width="1" style="display:none"
-  src="https://www.facebook.com/tr?id=143161029545703&ev=PageView&noscript=1"
-/></noscript>
-<!-- End Facebook Pixel Code -->
-<!-- gogole remarketing -->
-<!-- gogole remarketing -->
 
 <meta name="google-site-verification" content="yhBY75qhA2xA7ROIQj8zlDcgyNTHNS0Ot4zEnV8as44" />
