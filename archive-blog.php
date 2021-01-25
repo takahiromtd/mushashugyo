b9cf7fba56e9be049e9d7f1ef1b91779b9a03064
takahiromtd <takahiro.mtd@gmail.com>
2021-01-25 16:50:12 +0900

    update

--- archive-blog.php
+++ archive-blog.php
@@ -8,12 +8,17 @@ Template Name: archive-blog.php
 	<h2 class="text-center">コラム</h2>
 </div>
 <div class="container pt-5 pb-5">
-	<section id="news">
-		<div class="mb-5 col-12 col-sm-12 col-md-8 col-lg-8 col-xl-8">
+	<section id="blog">
+		<div class="mb-5 col-12 col-sm-12 col-md-4 col-lg-4 col-xl-4">
 			<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-				<article class="news-list">
-					<time><?php the_time("Y/n/j l"); ?></time>
-					<a href="<?php the_permalink(); ?>"><h3 class=""><?php echo mb_substr($post->post_title, 0, 70).'…'; ?></h3></a>
+				<article class="blog-list">
+					<a href="<?php the_permalink(); ?>">
+						<img class="img-fluid mx-auto d-block kiji" src="<?php echo post_custom('wpcf-blog-images'); ?>">
+					</a>
+					<div class="pb-3 pr-3 pl-3">
+						<a href="<?php the_permalink(); ?>"><h2 class=""><?php echo mb_substr($post->post_title, 0, 120).'…'; ?></h2></a>
+						<time >掲載日：<?php the_time("Y. n. j"); ?></time>
+					</div>
 				</article>
 			<?php endwhile; endif; ?>
 		</div>
