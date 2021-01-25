a48a68c1df1ed97169ea1c47f225b2f9947ce60c
takahiromtd <takahiro.mtd@gmail.com>
2021-01-25 16:55:19 +0900

    update

--- archive-blog.php
+++ archive-blog.php
@@ -9,20 +9,21 @@ Template Name: archive-blog.php
 </div>
 <div class="container pt-5 pb-5">
 	<section id="blog">
-		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-			<div class="mb-5 col-12 col-sm-12 col-md-4 col-lg-4 col-xl-4">
-				<article class="blog-list">
-					<a href="<?php the_permalink(); ?>">
-						<img class="img-fluid mx-auto d-block kiji" src="<?php echo post_custom('wpcf-blog-images'); ?>">
-					</a>
-					<div class="pb-3 pr-3 pl-3">
-						<a href="<?php the_permalink(); ?>"><h2 class=""><?php echo mb_substr($post->post_title, 0, 120).'…'; ?></h2></a>
-						<time >掲載日：<?php the_time("Y. n. j"); ?></time>
-					</div>
-				</article>
-			</div>
-		<?php endwhile; endif; ?>
-
+		<div class="row">
+			<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+				<div class="mb-5 col-12 col-sm-12 col-md-4 col-lg-4 col-xl-4">
+					<article class="blog-list">
+						<a href="<?php the_permalink(); ?>">
+							<img class="img-fluid mx-auto d-block kiji" src="<?php echo post_custom('wpcf-blog-images'); ?>">
+						</a>
+						<div class="pb-3 pr-3 pl-3">
+							<a href="<?php the_permalink(); ?>"><h2 class=""><?php echo mb_substr($post->post_title, 0, 120).'…'; ?></h2></a>
+							<time >掲載日：<?php the_time("Y. n. j"); ?></time>
+						</div>
+					</article>
+				</div>
+			<?php endwhile; endif; ?>
+		</div>
 	</section>
 </div>
 <?php get_footer(); ?>
